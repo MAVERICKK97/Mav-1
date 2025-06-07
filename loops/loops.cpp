362cfc5e149daf22fc3a6fb45c0a7df455555083
Shrijay Talap <shrijay2411@gmail.com>
2025-06-07 11:23:41 +0530

    Add files via upload

---
+++ loops/loops.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+// int main(){
+// int n,sum=0;
+// cout<<"Enter the number:";
+// cin>>n;
+
+// for(int i=1; i<=n; i++)
+// {
+//     if(i%2==0)
+//         continue; // Skip even numbers
+// sum+=i;
+   
+// }
+// cout<<"Sum of first "<<n<<" natural numbers is: "<<sum<<endl;
+// return 0;
+// }
+// int main(){
+//     int n = 10  ;
+//     int oddSum = 0;
+//     for (int i=1;i<=n;i++){
+//         if(i%2 != 0){
+//             oddSum += i;
+//            // Adding the value of i to oddSum for each odd number
+//     }
+    
+// }
+// cout<<"Sum of first odd natural numbers is: "<<oddSum<<endl;
+//     return 0;
+// }
+int main(){
+    for(int i=1;i<=5;i++)
+    {
+        int m=6;
+        for(int i=1;i<=m;i++)
+        {
+            cout<<"*";
+        }cout<<endl;    
+    }
+    }
